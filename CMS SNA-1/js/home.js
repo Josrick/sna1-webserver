7fa7db118942b0d1b205b5c52d6d696964baf067
Jombie <98839243+Josrick@users.noreply.github.com>
2024-03-17 17:13:56 +0800

    Add files via upload

---
+++ CMS SNA-1/js/home.js
@@ -0,0 +1,11 @@
+let Text = document.getElementById('text');
+let Sky = document.getElementById('Sky');
+let stars = document.getElementById('stars');
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+
+    Text.style.left = value * -2 + 'px';
+    Sky.style.top = value * -.5 + 'px';
+    stars.style.left = value * 1 + 'px';
+});
